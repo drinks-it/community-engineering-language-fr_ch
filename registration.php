567d9331ee3f8808d272eec496c169bcc963d5b8
Maxim <maxim.gubar@gmail.com>
2020-11-25 09:07:37 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -7,4 +7,4 @@ declare(strict_types=1);
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'drinks_it_communityengineering_fr_ch', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'drinksit_fr_ch', __DIR__);
